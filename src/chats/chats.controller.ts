2f5a99b2e2d6eb0729d82304de4e80e4eab75928
maindishes <65796820+maindishes@users.noreply.github.com>
2023-02-26 17:25:18 +0900

    feat : 채팅 관련 해서 일단 DB에 생성 되는지 확인.

---
+++ src/chats/chats.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { ChatsService } from './chats.service';
+import { User } from 'src/users/users.entity';
+import { Users } from 'src/common/decorators/user.decorator';
+@Controller('test')
+export class ChatsController {
+    constructor(private chatsService: ChatsService) {}
+
+    // @ApiOperation({ summary: 채팅방 모두 가져오기})
+    @Get(':url/chats')       // @User user:Users
+    async getChats(@Param('url') url, @Users() user:User) {
+        return this.chatsService.getChats(url, 1);
+    }
+
+    // @ApiOperation({ summary: 특정 채팅방  가져오기})
+
+    // @ApiOperation({ summary: 채팅방 만들기})
+    @Post(':url/chats/:id/contents')
+    async createChats(
+        @Param('url') url,
+        @Param('id') id:number, // ParseIntPipe 
+        @Body('content') content,
+        @Users() user:User,
+    ){
+        return this.chatsService.createChats(url,content,id,12)
+    }
+
+}
