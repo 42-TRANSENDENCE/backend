52bbb4e79e48dafe753bc292f7991373ed5f5829
maindishes <65796820+maindishes@users.noreply.github.com>
2023-02-26 20:48:26 +0900

    chore: 프론트와 연결 확인을 위한 url 변경

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -6,18 +6,18 @@ import { Users } from 'src/common/decorators/user.decorator';
 import { CreateChannelDto } from './dto/create-chat.dto';
 
 @ApiTags('CHANNEL')
-@Controller('channels')
+@Controller('api/room_list')
 export class ChannelsController {
     constructor(private channelsService: ChannelsService) {}
 
     @ApiOperation({ summary: '채팅방 모두 가져오기'})
-    @Get(':url/channels')
+    @Get('/')
     async getChannels(@Param('url',) url, @Users() user: User) {
         return this.channelsService.getChannels(url,1);
     }
 
     @ApiOperation({ summary: '채널 만들기'})
-    @Post(':url/channels')
+    @Post('/room')
     async createChannels(
         @Param('url') url,
         @Body() body: CreateChannelDto,
