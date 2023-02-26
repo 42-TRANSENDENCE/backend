dc226dc25e8cbf0ba81b81df7b57fb3f28ba6c0a
maindishes <65796820+maindishes@users.noreply.github.com>
2023-02-26 17:23:55 +0900

    chore: port 도 나중에 환경변수로 관리

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000); // 환경변수로 설정하게끔 나중에 바꾸자.
 }
 bootstrap();
