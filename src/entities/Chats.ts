f5ab74eeddb1fb68c615a4d8bbfe5260da237f1f
maindishes <65796820+maindishes@users.noreply.github.com>
2023-02-26 17:18:45 +0900

    add : add folder to handle entities and add chats entitiy file

---
+++ src/entities/Chats.ts
@@ -0,0 +1,30 @@
+import { PrimaryGeneratedColumn, 
+    Column, 
+    CreateDateColumn,
+    UpdateDateColumn,
+    Entity,
+ } from "typeorm";
+
+@Entity({ schema: 'j_test', name: 'chats' })
+export class Chats {
+    @PrimaryGeneratedColumn({type:'int', name: 'id'})
+    id : number;
+
+    @Column('text', { name: 'content' })
+    content: string;
+  
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @UpdateDateColumn()
+    updatedAt: Date;
+  
+    @Column('int', { name: 'WorkspaceId', nullable: true })
+    WorkspaceId: number | null;
+  
+    @Column('int', { name: 'SenderId', nullable: true })
+    SenderId: number | null;
+  
+    @Column('int', { name: 'ReceiverId', nullable: true })
+    ReceiverId: number | null;   
+}
