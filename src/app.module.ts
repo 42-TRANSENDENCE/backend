86afc300916f476af535f52463b85b57be0d45ff
Minjune <minjune8506@gmail.com>
2023-03-02 14:09:28 +0900

    refact: APP_PIPE const 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -24,7 +25,7 @@ import { UsersModule } from './users/users.module';
   providers: [
     AppService,
     {
-      provide: 'APP_PIPE',
+      provide: APP_PIPE,
       useClass: ValidationPipe,
     },
   ],
