c65dbc0c96295beffed596991fe7feed199c1d99
Minjune <minjune8506@gmail.com>
2023-03-07 17:30:54 +0900

    feat: Swagger API 문서화

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import {
-  Body,
   ClassSerializerInterceptor,
   Controller,
   Get,
@@ -9,7 +8,13 @@ import {
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiCookieAuth,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 import { FourtyTwoGuard } from './guards/fourty-two.guard';
@@ -27,10 +32,15 @@ export class AuthController {
 
   @Post('login')
   @UseGuards(FourtyTwoGuard)
+  @ApiOkResponse({
+    description:
+      '로그인 성공 cookie 설정 사용자 정보를 반환하지 않으면 2차인증 필요',
+  })
+  @ApiBearerAuth('42-token')
+  @ApiNotFoundResponse({ description: '회원가입 필요' })
   @HttpCode(200)
   async login(@Req() req) {
     const { id } = req.user;
-
     const user = await this.usersService.getById(id);
 
     const accessCookie = this.authService.getCookieWithJwtAccessToken(id);
@@ -38,16 +48,21 @@ export class AuthController {
       this.authService.getCookieWithJwtRefreshToken(id);
 
     this.usersService.setCurrentRefreshToken(refreshToken, id);
-    req.res.setHeader('Set-Cookie', [accessCookie, refreshCookie]);
 
+    req.res.setHeader('Set-Cookie', [accessCookie, refreshCookie]);
     if (!user.isTwoFactorAuthenticationEnabled) {
       return user;
     }
-    return;
+    return user;
   }
 
   @Get('refresh')
   @UseGuards(JwtRefreshAuthGuard)
+  @ApiOkResponse({
+    description:
+      'refresh token을 기반으로 새로운 access token을 cookie에 저장 기존 token이 만료되었을때 사용',
+  })
+  @ApiCookieAuth('Refresh')
   refresh(@Req() req) {
     const { id } = req.user;
     const accessTokenCookie = this.authService.getCookieWithJwtAccessToken(id);
@@ -57,6 +72,8 @@ export class AuthController {
   @Post('logout')
   @UseGuards(JwtAuthGuard)
   @HttpCode(200)
+  @ApiOkResponse({ description: '로그아웃. cookie token 삭제' })
+  @ApiCookieAuth('Authentication')
   async logOut(@Req() req) {
     const { id } = req.user;
     await this.usersService.removeRefreshToken(id);
--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -19,6 +19,7 @@ export class User {
     type: 'bytea',
     nullable: false,
   })
+  @Exclude()
   avatar: Uint8Array;
 
   @Column({
