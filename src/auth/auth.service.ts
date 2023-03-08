a7e963bf57b06fdfe904dee63718ff0482d038a1
Minjune <minjune8506@gmail.com>
2023-03-08 14:11:41 +0900

    refactor: 사용하지 않는 import 제거

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '../users/users.entity';
-import { Repository } from 'typeorm';
-
 import { catchError, firstValueFrom } from 'rxjs';
 import { AxiosError } from 'axios';
 import { HttpService } from '@nestjs/axios';
