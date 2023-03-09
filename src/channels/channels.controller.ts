3dd0414e11d504b346173d495a381eb214fef151
maindishes <65796820+maindishes@users.noreply.github.com>
2023-03-09 18:17:45 +0900

    feat: 채널방 비밀번호 해쉬화

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -4,7 +4,7 @@ import { ChannelsService } from './channels.service';
 import { User } from 'src/users/users.entity';
 import { Users } from 'src/common/decorators/user.decorator';
 import { CreateChannelDto } from './dto/create-channel.dto';
-
+import { Socket} from 'socket.io';
 @ApiTags('CHANNEL')
 @Controller('api/room_list')
 export class ChannelsController {
@@ -24,18 +24,29 @@ export class ChannelsController {
         // @Param('url') url,
         @Body() body: CreateChannelDto,
         @Users() user: User,
+        // @Socket() client: Socket,
     ){
         // 처음 방을 만드는 유저의 아이디 에 해당 하는 닉네임을 보여줘야 한다.
         // 마지막 인자 1 -> user.nickname 으로 나중에 
-        return this.channelsService.createChannels(body.title, body.password, 1);
+        return this.channelsService.createChannels(body.title, body.password, 2);
     }
 
-    @ApiOperation({ summary: '채팅방 멤버 가져오기'})
-    @Get('/room/:title')
+    @ApiOperation({ summary: '채팅방 정보 가져오기: [멤버, 밴리스트, private]'})
+    @Get('/room/:channelId')
     async getChannelMembers(
-        @Param('title') title: string,
+        @Param('channelId') channelId: number,
     ){
-        return this.channelsService.getChannelMembers(title);
+        return this.channelsService.getChannelMembers(channelId);
+    }
+
+    @ApiOperation({ summary: '채팅방 입장'})
+    @Post('/room/:channelId')
+    async userEnterChannel(
+        @Param('channelId') channelId : number,
+        @Body() body: CreateChannelDto,
+        @Users() user : User,
+    ) {
+        return this.channelsService.userEnterChannel(channelId,body.password,user)
     }
 
 }
