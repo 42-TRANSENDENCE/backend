ab013f60d7edc819b601435c4bb3bacb6b5632ac
maindishes <65796820+maindishes@users.noreply.github.com>
2023-03-16 15:49:50 +0900

    Chore: Channels 모듈 추가

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -4,8 +4,10 @@ import { ChatsController } from './chats.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/users.entity';
 import { Chats } from 'src/chats/chats.entity';
+import { EventsModule } from 'src/events/events.module';
+import { Channels } from 'src/channels/channels.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Chats, User])] , // EventsMoudle
+  imports: [TypeOrmModule.forFeature([Chats, User, Channels]) , EventsModule],
   providers: [ChatsService],
   controllers: [ChatsController]
 })
