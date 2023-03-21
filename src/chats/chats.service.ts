277d63036fb74121f99ed7aaa4b3696d11f5f0a8
maindishes <65796820+maindishes@users.noreply.github.com>
2023-03-21 21:34:34 +0900

    Feat: MuteMember에 있으면 채팅 저장 하지 않기 #72
    - 소켓 관련해서는 구현중 ...
    - TODO: 채팅창 연결해서 User.id랑 연결해서 테스트
    - TODO: 인자들 정리, entity 도 정리

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Chats } from './chats.entity';
 import { User } from 'src/users/users.entity';
 import { Channels } from 'src/channels/channels.entity';
 import { ChannelsGateway } from 'src/channels/events.chats.gateway';
+import { ChannelMuteMember } from 'src/channels/channelmutemember.entity';
+import { ChannelsService } from 'src/channels/channels.service';
 
 // function getKeyByValue(object, value) {
 //   return Object.keys(object).find((key) => object[key] === value);
@@ -14,6 +16,12 @@ import { ChannelsGateway } from 'src/channels/events.chats.gateway';
 export class ChatsService {
   constructor(
     @InjectRepository(Chats) private chatsRepository: Repository<Chats>,
+    @InjectRepository(User) private usersRepository: Repository<User>,
+    @InjectRepository(Channels)
+    private channelsRepository: Repository<Channels>,
+    @InjectRepository(ChannelMuteMember)
+    private channelMuteRpository: Repository<ChannelMuteMember>,
+    private readonly channelsService: ChannelsService,
     private readonly channelsGateway: ChannelsGateway,
   ) {}
 
@@ -42,13 +50,17 @@ export class ChatsService {
     // relations:['Sender'],
     // this.eventsGateway.server.to(receiverSocketId).emit('dm', dmWithSender);
   }
-  async sendChatToChannel(id: number, chat: string, user: User) {
+  //TODO: 채팅창 연결해서 User.id랑 연결해서 테스트 , 인자들 정리, entity 도 정리
+  async sendChatToChannel(roomId: number, chat: string, user: User) {
+    console.log(await this.channelsService.isMutted(roomId, 2));
+    if (await this.channelsService.isMutted(roomId, 2))
+      throw new UnauthorizedException('YOU ARE MUTTED');
     const chats = this.chatsRepository.create({
-      SenderId: 2, // user.id
-      ChannelId: id,
+      SenderId: 2, //user.id,
+      ChannelId: roomId,
       content: chat,
     });
     const saveChat = await this.chatsRepository.save(chats);
-    this.channelsGateway.sendEmitMessage(saveChat);
+    this.channelsGateway.nsp.emit('message', chat);
   }
 }
