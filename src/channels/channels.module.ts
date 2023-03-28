891ac507e266c95918bb3eeffbf20f3a8baa3992
maindishes <65796820+maindishes@users.noreply.github.com>
2023-03-28 11:44:33 +0900

    Chore: module import

--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -6,9 +6,10 @@ import { Channels } from 'src/channels/channels.entity';
 import { User } from 'src/users/users.entity';
 import { ChannelMember } from 'src/channels/channelcember.entity';
 import { EventsModule } from 'src/events/events.module';
+import { Chats } from 'src/chats/chats.entity';
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Channels, User, ChannelMember]),
+    TypeOrmModule.forFeature([Channels, User, ChannelMember, Chats]),
     EventsModule,
   ],
   providers: [ChannelsService],
