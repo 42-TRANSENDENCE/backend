b5529f5f4f44665e8d36743f81af4e336be2ee07
maindishes <65796820+maindishes@users.noreply.github.com>
2023-03-31 12:36:33 +0900

    Chore :  events파일 channel 하위로 이동

--- src/channels/events.module.ts
+++ src/channels/events.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ChannelsModule } from 'src/channels/channels.module';
-import { ChannelsGateway } from './events.channels.gateway';
+import { ChannelsGateway } from './events.chats.gateway';
 import { forwardRef } from '@nestjs/common';
 @Module({
   imports: [forwardRef(() => ChannelsModule)],
