84409ac623e54dbe372422cfbc77dc7690317e5a
maindishes <65796820+maindishes@users.noreply.github.com>
2023-04-12 17:03:53 +0900

    Fix : 채널 멤버의 상태를 enum으로 관리

--- src/channels/channelmember.entity.ts
+++ src/channels/channelmember.entity.ts
@@ -13,6 +13,12 @@ import {
 } from 'typeorm';
 import { Channel } from './channels.entity';
 
+export enum MemberType {
+  OWNER = 'OWNER',
+  ADMIN = 'ADMIN',
+  MEMBER = 'MEMBER',
+}
+
 @Entity({ name: 'channel_member' })
 export class ChannelMember {
   @PrimaryColumn()
@@ -20,6 +26,9 @@ export class ChannelMember {
 
   @PrimaryColumn()
   channelId: number;
+
+  @Column({ type: 'enum', enum: MemberType })
+  type: MemberType;
 
   @ManyToOne(() => User, (user) => user.memberchannels, { onDelete: 'CASCADE' })
   user: User;
