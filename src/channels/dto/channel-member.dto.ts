34e6ce9b23ee66a6f86240f9ab6beddbb9738151
maindishes <65796820+maindishes@users.noreply.github.com>
2023-04-15 15:52:00 +0900

    Chore : MemberDto 구현중..

---
+++ src/channels/dto/channel-member.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ChannelMember, MemberType } from '../channelmember.entity';
+import { ChannelStatus } from '../channels.entity';
+import { User } from 'src/users/users.entity';
+import { Channel } from 'diagnostics_channel';
+export class ChannelMemberDto {
+  @ApiProperty()
+  userId: number;
+
+  @ApiProperty()
+  channelId: number;
+
+  @ApiProperty()
+  nickname: string;
+
+  @ApiProperty()
+  type: MemberType;
+
+  @ApiProperty()
+  avatar: Uint8Array;
+
+  constructor(member: ChannelMember) {
+    this.userId = member.userId;
+    this.channelId = member.channelId;
+    console.log(member.user);
+    this.nickname = member.user.nickname;
+    this.type = member.type;
+    this.avatar = member.user.avatar;
+  }
+}
