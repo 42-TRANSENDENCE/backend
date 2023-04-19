87a9fa64155852ab9ad23eb5fb2343b2365faec4
maindishes <65796820+maindishes@users.noreply.github.com>
2023-04-19 14:57:46 +0900

    Chore : key : value 로 보내기

--- src/channels/events.chats.gateway.ts
+++ src/channels/events.chats.gateway.ts
@@ -143,7 +143,11 @@ export class ChannelsGateway
   }
 
   async emitOutMember(userId: number, channelId: number) {
-    this.nsp.to(channelId.toString()).emit('outMember');
+    this.nsp.to(channelId.toString()).emit('outMember', { userId: userId });
+  }
+
+  async emitMuteMember(userId: number, channelId: number) {
+    this.nsp.to(channelId.toString()).emit('muteMember', { userId: userId });
   }
 
   async EmitChannelInfo(channelReturned) {
