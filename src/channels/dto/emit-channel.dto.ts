68bd0924ad69aa7019a9eb578163cda396c00821
maindishes <65796820+maindishes@users.noreply.github.com>
2023-04-21 13:27:28 +0900

    Chore  : interface 추가

--- src/channels/dto/emit-channel.dto.ts
+++ src/channels/dto/emit-channel.dto.ts
@@ -3,6 +3,11 @@ import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { ChannelMember, MemberType } from '../entity/channelmember.entity';
 import { ChannelStatus } from '../entity/channels.entity';
 
+export interface HowMany {
+  connectedSocket: number;
+  joinMember: number;
+}
+
 export class EmitChannelInfoDto {
   @ApiProperty()
   id: number;
@@ -20,13 +25,18 @@ export class EmitChannelInfoDto {
   createdAt: Date;
 
   @ApiProperty()
+  howMany: HowMany;
+
+  @ApiProperty()
   updatedAt: Date;
-  constructor(channel) {
+
+  constructor(channel, HowMany) {
     this.id = channel.id;
     this.title = channel.title;
     this.owner = channel.owner;
     this.status = channel.status;
     this.createdAt = channel.createdAt;
     this.updatedAt = channel.updatedAt;
+    this.howMany = HowMany;
   }
 }
