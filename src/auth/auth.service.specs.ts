c6f1c7e7b1b88a6d91c040a40178aa319478ff8d
maindishes <65796820+maindishes@users.noreply.github.com>
2023-04-28 20:11:01 +0900

    Temp : 이름 다르게 해서 저장해놓기

---
+++ src/auth/auth.service.specs.ts
@@ -0,0 +1,71 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from 'src/users/users.service';
+import { mockedConfigService } from 'src/utils/mocks/config.service';
+import { mockedJwtService } from 'src/utils/mocks/jwt.service';
+import { AuthService } from './auth.service';
+import { AppModule } from 'src/app.module';
+import { AuthModule } from './auth.module';
+import { UsersModule } from 'src/users/users.module';
+
+describe('AuthService', () => {
+  let authService: AuthService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports:[AppModule,UsersModule, AuthModule],
+      providers: [AuthService, JwtService, ConfigService, UsersService],
+    })
+      .overrideProvider(JwtService)
+      .useValue(mockedJwtService)
+      .overrideProvider(ConfigService)
+      .useValue(mockedConfigService)
+      .overrideProvider(UsersService)
+      .useValue({})
+      .compile();
+
+    authService = module.get<AuthService>(AuthService);
+  });
+
+  describe('create cookie with JWT', () => {
+    const id = 42;
+    const accessTokenExpirationTime = mockedConfigService.get(
+      'JWT_ACCESS_TOKEN_EXPIRATION_TIME',
+    );
+    const refreshTokenExpirationTime = mockedConfigService.get(
+      'JWT_REFRESH_TOKEN_EXPIRATION_TIME',
+    );
+
+    it('access token cookie should valid', () => {
+      const result = authService.getCookieWithJwtAccessToken(id);
+
+      expect(result).toBeDefined();
+      expect(result).toMatch(/Authentication=.+;/);
+      expect(result).toMatch(/HttpOnly; Path=\/;/);
+      expect(result).toMatch(
+        new RegExp(`Max-Age=${accessTokenExpirationTime}`),
+      );
+    });
+
+    it('refresh token cookie should valid', () => {
+      const result = authService.getCookieWithJwtRefreshToken(id);
+
+      expect(result.refreshCookie).toBeDefined();
+      expect(result.refreshCookie).toMatch(/Refresh=.+;/);
+      expect(result.refreshCookie).toMatch(/HttpOnly; Path=\/;/);
+      expect(result.refreshCookie).toMatch(
+        new RegExp(`Max-Age=${refreshTokenExpirationTime}`),
+      );
+    });
+  });
+
+  describe('get cookie for logout', () => {
+    it('should return an array of cookies for logout', () => {
+      const result = authService.getCookieForLogOut();
+
+      expect(result).toBeInstanceOf(Array);
+      expect(result[0]).toMatch(/Authentication=;/);
+      expect(result[1]).toMatch(/Refresh=;/);
+    });
+  });
+});
